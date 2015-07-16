73001c58ca9334092607946d9ed2b58cacc15bd7
phoudek <phoudek@irm-network.com>
2015-07-16 08:41:54 +0200

    added copyright for example files

--- sdk/sdk/src/main/java/org/zgif/samples/TransformerExampleMain.java
+++ sdk/sdk/src/main/java/org/zgif/samples/TransformerExampleMain.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Martin Fluegge (Berlin, Germany) and others..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Martin Fluegge - initial API and implementation and/or initial documentation
+ *  
+ *******************************************************************************/
 package org.zgif.samples;
 
 import java.util.List;
@@ -15,40 +26,40 @@ import org.zgif.samples.transformer.UserTransformer;
  */
 public class TransformerExampleMain {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		User user = new User();
+        User user = new User();
 
-		user.setFirstName("Martin");
-		user.setLastName("Flügge");
-		user.setSex(Sex.MALE);
+        user.setFirstName("Martin");
+        user.setLastName("Flügge");
+        user.setSex(Sex.MALE);
 
-		UserTransformer userTransformer = new UserTransformer();
-		userTransformer.setValidate(false);
-		Nutzer nutzer;
+        UserTransformer userTransformer = new UserTransformer();
+        userTransformer.setValidate(false);
+        Nutzer nutzer;
 
-		try {
-			nutzer = userTransformer.transform(user, null);
-		} catch (MapperValidationException e) {
-			throw new RuntimeException();
-		}
+        try {
+            nutzer = userTransformer.transform(user, null);
+        } catch (MapperValidationException e) {
+            throw new RuntimeException();
+        }
 
-		System.out.println(nutzer.getVorName());
-		System.out.println(nutzer.getNachName());
-		System.out.println(nutzer.getGeschlecht());
+        System.out.println(nutzer.getVorName());
+        System.out.println(nutzer.getNachName());
+        System.out.println(nutzer.getGeschlecht());
 
-		userTransformer.setValidate(true);
-		user.setFirstName(null);
-		user.setSex(null);
+        userTransformer.setValidate(true);
+        user.setFirstName(null);
+        user.setSex(null);
 
-		try {
-			nutzer = userTransformer.transform(user, null);
-		} catch (MapperValidationException e) {
+        try {
+            nutzer = userTransformer.transform(user, null);
+        } catch (MapperValidationException e) {
 
-			List<ValidationError> results = e.getResults();
-			for (ValidationError validationError : results) {
-				System.out.println(validationError.getMessage());
-			}
-		}
-	}
+            List<ValidationError> results = e.getResults();
+            for (ValidationError validationError : results) {
+                System.out.println(validationError.getMessage());
+            }
+        }
+    }
 }
--- sdk/sdk/src/main/java/org/zgif/samples/validator/converter/CSVToObjectConverter.java
+++ sdk/sdk/src/main/java/org/zgif/samples/validator/converter/CSVToObjectConverter.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Martin Fluegge (Berlin, Germany) and others..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Martin Fluegge - initial API and implementation and/or initial documentation
+ *  
+ *******************************************************************************/
 package org.zgif.samples.validator.converter;
 
 import java.io.BufferedReader;
@@ -17,61 +28,61 @@ import org.zgif.samples.model.a.User;
  */
 public class CSVToObjectConverter extends BasicConverter {
 
-	@Override
-	public void doConvertData(IConverterDescriptor descriptor) {
+    @Override
+    public void doConvertData(IConverterDescriptor descriptor) {
 
-		CSVToObjectConverterDescriptor descr = (CSVToObjectConverterDescriptor) descriptor;
+        CSVToObjectConverterDescriptor descr = (CSVToObjectConverterDescriptor) descriptor;
 
-		String pathToCSV = descr.getPathToCSV();
+        String pathToCSV = descr.getPathToCSV();
 
-		convert(pathToCSV);
-	}
+        convert(pathToCSV);
+    }
 
-	private void convert(String pathToCSV) {
+    private void convert(String pathToCSV) {
 
-		try {
-		    BufferedReader br = new BufferedReader(new FileReader(pathToCSV));
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(pathToCSV));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-			while (line != null) {
-				sb.append(line);
-				sb.append(System.getProperty("line.separator"));
-				line = br.readLine();
-				if (line == null) {
-					continue;
-				}
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.getProperty("line.separator"));
+                line = br.readLine();
+                if (line == null) {
+                    continue;
+                }
 
-				User user = getTransformer().transform(line.split(","), null);
+                User user = getTransformer().transform(line.split(","), null);
 
-				System.out.println(user.getFirstName() + " : " + user.getLastName() + " : " + user.getSex());
-			}
-			String everything = sb.toString();
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException();
-		} catch (IOException e) {
-			throw new RuntimeException();
-		} catch (MapperValidationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+                System.out.println(user.getFirstName() + " : " + user.getLastName() + " : " + user.getSex());
+            }
+            String everything = sb.toString();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        } catch (MapperValidationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public ITransformer<String[], User> getTransformer() {
+    @Override
+    public ITransformer<String[], User> getTransformer() {
 
-		return new CSVToUserTransformer();
-	}
+        return new CSVToUserTransformer();
+    }
 
-	public static class CSVToObjectConverterDescriptor implements IConverterDescriptor {
-		private String pathToCSV;
+    public static class CSVToObjectConverterDescriptor implements IConverterDescriptor {
+        private String pathToCSV;
 
-		public String getPathToCSV() {
-			return pathToCSV;
-		}
+        public String getPathToCSV() {
+            return pathToCSV;
+        }
 
-		public void setPathToCSV(String pathToCSV) {
-			this.pathToCSV = pathToCSV;
-		}
-	}
+        public void setPathToCSV(String pathToCSV) {
+            this.pathToCSV = pathToCSV;
+        }
+    }
 }
