d699b04c55420bac1a51899eb36e96318c0b7c55
mfluegge <martin.fluegge@gmx.de>
2016-08-11 16:15:59 +0200

    Fixed compile inconsistencies

--- sdk/sdk/src/main/java/org/zgif/converter/plugin/output/shellprinter/ShellPrinter.java
+++ sdk/sdk/src/main/java/org/zgif/converter/plugin/output/shellprinter/ShellPrinter.java
@@ -27,7 +27,7 @@ import org.zgif.model.datatype.enumeration.Subset;
 import org.zgif.model.node.AbstractNode;
 import org.zgif.model.node.Data;
 import org.zgif.model.node.Meta;
-import org.zgif.model.node.Periods.Period;
+import org.zgif.model.node.Period;
 import org.zgif.model.node.ZGif;
 import org.zgif.model.node.entity.AbstractEntityNode;
 
@@ -38,189 +38,192 @@ import org.zgif.model.node.entity.AbstractEntityNode;
  * 
  */
 public class ShellPrinter implements IExportPlugin {
-    private static Logger        logger            = Logger.getLogger(ShellPrinter.class);
-    public static final Subset[] SUPPORTED_SUBSETS = Subset.values();
+	private static Logger logger = Logger.getLogger(ShellPrinter.class);
+	public static final Subset[] SUPPORTED_SUBSETS = Subset.values();
 
-    protected ZGif               zgif              = null;
+	protected ZGif zgif = null;
 
-    private String               prefix            = "";
+	private String prefix = "";
 
-    /**
-     * returns the list of supported subsets of this import plugin
-     * 
-     * @author phoudek
-     * @return list of supported subsets
-     */
-    @Override
-    public List<Subset> getSupportedSubsets() {
-        return Arrays.asList(SUPPORTED_SUBSETS);
-    }
+	/**
+	 * returns the list of supported subsets of this import plugin
+	 * 
+	 * @author phoudek
+	 * @return list of supported subsets
+	 */
+	@Override
+	public List<Subset> getSupportedSubsets() {
+		return Arrays.asList(SUPPORTED_SUBSETS);
+	}
 
-    private void doExport() {
-        exportMeta(zgif.getMeta());
+	private void doExport() {
+		exportMeta(zgif.getMeta());
 
-        System.out.println(prefix + "Data:");
-        Method getPeriods = null;
-        try {
-            getPeriods = zgif.getClass().getMethod("getPeriods");
-        } catch (SecurityException e) {
-        } catch (NoSuchMethodException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        if (getPeriods != null) {
-            try {
-                @SuppressWarnings("unchecked")
-                Map<String, Period> periods = (Map<String, Period>) getPeriods.invoke(zgif);
-                if (periods != null) {
-                    exportPeriods(periods.values());
-                }
-            } catch (InvocationTargetException e) {
-                logger.error("", e);
-            } catch (IllegalArgumentException e) {
-                logger.error("", e);
-            } catch (IllegalAccessException e) {
-                logger.error("", e);
-            }
-        }
+		System.out.println(prefix + "Data:");
+		Method getPeriods = null;
+		try {
+			getPeriods = zgif.getClass().getMethod("getPeriods");
+		} catch (SecurityException e) {
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (getPeriods != null) {
+			try {
+				@SuppressWarnings("unchecked")
+				Map<String, Period> periods = (Map<String, Period>) getPeriods.invoke(zgif);
+				if (periods != null) {
+					exportPeriods(periods.values());
+				}
+			} catch (InvocationTargetException e) {
+				logger.error("", e);
+			} catch (IllegalArgumentException e) {
+				logger.error("", e);
+			} catch (IllegalAccessException e) {
+				logger.error("", e);
+			}
+		}
 
-        Method getData = null;
-        try {
-            getData = zgif.getClass().getMethod("getData");
-        } catch (SecurityException e) {
-            logger.error("", e);
-        } catch (NoSuchMethodException e) {
-            logger.error("", e);
-        }
-        if (getData != null) {
-            try {
-                Data data = (Data) getData.invoke(zgif);
-                if (data != null) {
-                    exportNode(data);
-                }
-            } catch (InvocationTargetException e) {
-                logger.error("", e);
-            } catch (IllegalArgumentException e) {
-                logger.error("", e);
-            } catch (IllegalAccessException e) {
-                logger.error("", e);
-            }
-        }
-    }
+		Method getData = null;
+		try {
+			getData = zgif.getClass().getMethod("getData");
+		} catch (SecurityException e) {
+			logger.error("", e);
+		} catch (NoSuchMethodException e) {
+			logger.error("", e);
+		}
+		if (getData != null) {
+			try {
+				Data data = (Data) getData.invoke(zgif);
+				if (data != null) {
+					exportNode(data);
+				}
+			} catch (InvocationTargetException e) {
+				logger.error("", e);
+			} catch (IllegalArgumentException e) {
+				logger.error("", e);
+			} catch (IllegalAccessException e) {
+				logger.error("", e);
+			}
+		}
+	}
 
-    private void exportPeriods(Collection<Period> periods) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        if (periods != null && periods.size() > 0) {
-            prefix += "  ";
-            // System.out.println(prefix + periods.size() + "x Periods:");
-            for (Period period : periods) {
-                System.out.println(prefix + "- Period " + period.toString() + " (" + period.getIdentifier() + "; " + period.getFrom() + "->" + period.getTo()
-                    + ")");
-                // TODO
-                // exportNode(period.getData());
-            }
-            prefix = prefix.substring(2);
-        }
-    }
+	private void exportPeriods(Collection<Period> periods)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		if (periods != null && periods.size() > 0) {
+			prefix += "  ";
+			// System.out.println(prefix + periods.size() + "x Periods:");
+			for (Period period : periods) {
+				System.out.println(prefix + "- Period " + period.toString() + " (" + period.getIdentifier() + "; "
+						+ period.getFrom() + "->" + period.getTo() + ")");
+				// TODO
+				// exportNode(period.getData());
+			}
+			prefix = prefix.substring(2);
+		}
+	}
 
-    private void exportNode(AbstractNode node) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        System.out.print(prefix + " - " + node.getClass().getSimpleName());
-        if (node instanceof AbstractEntityNode) {
-            AbstractEntityNode eNode = (AbstractEntityNode) node;
-            System.out.print(" (id=" + eNode.getObjectIdSender() + "; label=" + eNode.getLabel() + ")");
-        }
-        System.out.println();
+	private void exportNode(AbstractNode node)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		System.out.print(prefix + " - " + node.getClass().getSimpleName());
+		if (node instanceof AbstractEntityNode) {
+			AbstractEntityNode eNode = (AbstractEntityNode) node;
+			System.out.print(" (id=" + eNode.getObjectIdSender() + "; label=" + eNode.getLabel() + ")");
+		}
+		System.out.println();
 
-        Method[] methods = node.getClass().getDeclaredMethods();
-        for (Method method : methods) {
-            String methodName = method.getName();
-            if (methodName.startsWith("getListOf")) {
-                @SuppressWarnings("unchecked")
-                Map<String, AbstractNode> nodes = (Map<String, AbstractNode>) method.invoke(node);
-                if (nodes != null) {
-                    exportNodes(nodes.values());
-                }
-            }
-        }
-    }
+		Method[] methods = node.getClass().getDeclaredMethods();
+		for (Method method : methods) {
+			String methodName = method.getName();
+			if (methodName.startsWith("getListOf")) {
+				@SuppressWarnings("unchecked")
+				Map<String, AbstractNode> nodes = (Map<String, AbstractNode>) method.invoke(node);
+				if (nodes != null) {
+					exportNodes(nodes.values());
+				}
+			}
+		}
+	}
 
-    private void exportNodes(Collection<AbstractNode> nodes) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        if (nodes != null && !nodes.isEmpty()) {
-            prefix += "  ";
+	private void exportNodes(Collection<AbstractNode> nodes)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		if (nodes != null && !nodes.isEmpty()) {
+			prefix += "  ";
 
-            // AbstractNode firstNode = nodes.iterator().next();
-            // System.out.println(prefix + nodes.size() + "x " +
-            // firstNode.getClass().getSimpleName() + ":");
-            for (AbstractNode node : nodes) {
-                exportNode(node);
-            }
+			// AbstractNode firstNode = nodes.iterator().next();
+			// System.out.println(prefix + nodes.size() + "x " +
+			// firstNode.getClass().getSimpleName() + ":");
+			for (AbstractNode node : nodes) {
+				exportNode(node);
+			}
 
-            prefix = prefix.substring(2);
-        }
-    }
+			prefix = prefix.substring(2);
+		}
+	}
 
-    protected void exportMeta(Meta meta) {
-        if (meta != null) {
-            System.out.println("Meta:");
-            System.out.println(" - format    = " + meta.getFormat());
-            System.out.println(" - subset    = " + meta.getProcess());
-            System.out.println(" - version   = " + meta.getVersion());
-            System.out.println(" - creator   = " + meta.getCreator());
-            System.out.println(" - publisher = " + meta.getPublisher());
-            System.out.println(" - created   = " + meta.getCreated());
-        }
-    }
+	protected void exportMeta(Meta meta) {
+		if (meta != null) {
+			System.out.println("Meta:");
+			System.out.println(" - format    = " + meta.getFormat());
+			System.out.println(" - subset    = " + meta.getProcess());
+			System.out.println(" - version   = " + meta.getVersion());
+			System.out.println(" - creator   = " + meta.getCreator());
+			System.out.println(" - publisher = " + meta.getPublisher());
+			System.out.println(" - created   = " + meta.getCreated());
+		}
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.zgif.converter.plugin.output.ExportPlugin#load(org.zgif.converter
-     * .plugin.PluginConfiguration, org.zgif.model.node.AbstractZGif)
-     */
-    @Override
-    public void load(ExportPluginConfiguration config, ZGif zgif) {
-        this.zgif = zgif;
-        doExport();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.zgif.converter.plugin.output.ExportPlugin#load(org.zgif.converter
+	 * .plugin.PluginConfiguration, org.zgif.model.node.AbstractZGif)
+	 */
+	@Override
+	public void load(ExportPluginConfiguration config, ZGif zgif) {
+		this.zgif = zgif;
+		doExport();
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.output.ExportPlugin#unload()
-     */
-    @Override
-    public void unload() {
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.output.ExportPlugin#unload()
+	 */
+	@Override
+	public void unload() {
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.output.ExportPlugin#getConfigGui()
-     */
-    @Override
-    public PluginComponent<ExportPluginConfiguration> getConfigGui() {
-        return null;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.output.ExportPlugin#getConfigGui()
+	 */
+	@Override
+	public PluginComponent<ExportPluginConfiguration> getConfigGui() {
+		return null;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.IPlugin#load(org.zgif.converter.plugin.
-     * PluginConfiguration)
-     */
-    @Override
-    public void load(PluginConfiguration config) {
-        throw new RuntimeException("not allowed");
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.IPlugin#load(org.zgif.converter.plugin.
+	 * PluginConfiguration)
+	 */
+	@Override
+	public void load(PluginConfiguration config) {
+		throw new RuntimeException("not allowed");
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.output.IExportPlugin#
-     * getRequiredConfigurationArguments()
-     */
-    @Override
-    public ExportPluginConfiguration getRequiredConfigurationArguments() {
-        return new ExportPluginConfiguration();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.output.IExportPlugin#
+	 * getRequiredConfigurationArguments()
+	 */
+	@Override
+	public ExportPluginConfiguration getRequiredConfigurationArguments() {
+		return new ExportPluginConfiguration();
+	}
 }
--- sdk/sdk/src/main/java/org/zgif/converter/plugin/output/zgif/ZGifWriter.java
+++ sdk/sdk/src/main/java/org/zgif/converter/plugin/output/zgif/ZGifWriter.java
@@ -33,7 +33,7 @@ import org.zgif.converter.plugin.output.IExportPlugin;
 import org.zgif.model.datatype.enumeration.Subset;
 import org.zgif.model.node.Data;
 import org.zgif.model.node.Meta;
-import org.zgif.model.node.Periods.Period;
+import org.zgif.model.node.Period;
 import org.zgif.model.node.ZGif;
 
 /**
@@ -41,170 +41,170 @@ import org.zgif.model.node.ZGif;
  * 
  */
 public class ZGifWriter implements IExportPlugin {
-    private static Logger             logger            = Logger.getLogger(ZGifWriter.class);
+	private static Logger logger = Logger.getLogger(ZGifWriter.class);
 
-    public static final Subset[]      SUPPORTED_SUBSETS = Subset.values();
-    private static String             PARAMETER_NAME    = "zgif";
+	public static final Subset[] SUPPORTED_SUBSETS = Subset.values();
+	private static String PARAMETER_NAME = "zgif";
 
-    private ZGif                      zgif              = null;
-    private ZipOutputStream           zipOut;
-    private List<String>              fileList          = new ArrayList<String>();
-    private ExportPluginConfiguration config;
+	private ZGif zgif = null;
+	private ZipOutputStream zipOut;
+	private List<String> fileList = new ArrayList<String>();
+	private ExportPluginConfiguration config;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.output.ExportPlugin#getSupportedSubsets()
-     */
-    @Override
-    public List<Subset> getSupportedSubsets() {
-        return Arrays.asList(SUPPORTED_SUBSETS);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.output.ExportPlugin#getSupportedSubsets()
+	 */
+	@Override
+	public List<Subset> getSupportedSubsets() {
+		return Arrays.asList(SUPPORTED_SUBSETS);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.output.IExportPlugin#
-     * getRequiredConfigurationArguments()
-     */
-    @Override
-    public ExportPluginConfiguration getRequiredConfigurationArguments() {
-        return new ExportPluginConfiguration() {
-            {
-                Map<String, OutputStream> streams = getStreams();
-                streams.put(PARAMETER_NAME, null);
-            }
-        };
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.output.IExportPlugin#
+	 * getRequiredConfigurationArguments()
+	 */
+	@Override
+	public ExportPluginConfiguration getRequiredConfigurationArguments() {
+		return new ExportPluginConfiguration() {
+			{
+				Map<String, OutputStream> streams = getStreams();
+				streams.put(PARAMETER_NAME, null);
+			}
+		};
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.output.ExportPlugin#getConfigGui()
-     */
-    @Override
-    public PluginComponent<ExportPluginConfiguration> getConfigGui() {
-        // TODO: abhängigkeiten auflösen
-        // return new
-        // DefaultPluginGui<ExportPluginConfiguration>(getRequiredConfigurationArguments());
-        return null;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.output.ExportPlugin#getConfigGui()
+	 */
+	@Override
+	public PluginComponent<ExportPluginConfiguration> getConfigGui() {
+		// TODO: abhängigkeiten auflösen
+		// return new
+		// DefaultPluginGui<ExportPluginConfiguration>(getRequiredConfigurationArguments());
+		return null;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.IPlugin#load(org.zgif.converter.plugin.
-     * PluginConfiguration)
-     */
-    @Override
-    public void load(PluginConfiguration config) {
-        throw new RuntimeException("not allowed");
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.IPlugin#load(org.zgif.converter.plugin.
+	 * PluginConfiguration)
+	 */
+	@Override
+	public void load(PluginConfiguration config) {
+		throw new RuntimeException("not allowed");
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.zgif.converter.plugin.output.ExportPlugin#load(org.zgif.converter
-     * .plugin.PluginConfiguration, org.zgif.model.node.AbstractZGif)
-     */
-    @Override
-    public void load(ExportPluginConfiguration config, ZGif zgif) {
-        this.zgif = zgif;
-        this.config = config;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.zgif.converter.plugin.output.ExportPlugin#load(org.zgif.converter
+	 * .plugin.PluginConfiguration, org.zgif.model.node.AbstractZGif)
+	 */
+	@Override
+	public void load(ExportPluginConfiguration config, ZGif zgif) {
+		this.zgif = zgif;
+		this.config = config;
 
-        doExport();
-    }
+		doExport();
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.zgif.converter.plugin.output.ExportPlugin#unload()
-     */
-    @Override
-    public void unload() {
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.zgif.converter.plugin.output.ExportPlugin#unload()
+	 */
+	@Override
+	public void unload() {
+	}
 
-    // //////////////////////////////////////////////////////////
+	// //////////////////////////////////////////////////////////
 
-    @SuppressWarnings({ "unused", "unchecked" })
-    private void doExport() {
-        zipOut = new ZipOutputStream(config.getStreams().get(PARAMETER_NAME));
-        zipOut.setLevel(ZipOutputStream.STORED);
-        try {
+	@SuppressWarnings({ "unused", "unchecked" })
+	private void doExport() {
+		zipOut = new ZipOutputStream(config.getStreams().get(PARAMETER_NAME));
+		zipOut.setLevel(ZipOutputStream.STORED);
+		try {
 
-            Meta meta = zgif.getMeta();
-            nextZipEntry("mimetype");
-            zipOut.write(meta.getFormat().getBytes());
-            zipOut.closeEntry();
+			Meta meta = zgif.getMeta();
+			nextZipEntry("mimetype");
+			zipOut.write(meta.getFormat().getBytes());
+			zipOut.closeEntry();
 
-            nextZipEntry("type");
-            zipOut.write("XML".getBytes());
-            zipOut.closeEntry();
+			nextZipEntry("type");
+			zipOut.write("XML".getBytes());
+			zipOut.closeEntry();
 
-            zipOut.setLevel(ZipOutputStream.DEFLATED);
+			zipOut.setLevel(ZipOutputStream.DEFLATED);
 
-            nextZipEntry("meta.xml");
-            MetaWriter metaWriter = new MetaWriter(zipOut);
-            metaWriter.write(meta);
+			nextZipEntry("meta.xml");
+			MetaWriter metaWriter = new MetaWriter(zipOut);
+			metaWriter.write(meta);
 
-            Method getData = null;
-            Data data = null;
-            try {
-                getData = zgif.getClass().getMethod("getData");
-                data = (Data) getData.invoke(zgif);
-            } catch (Exception e) {
-            }
-            if (data != null) {
-                exportData(data, "maindata.xml");
-            }
+			Method getData = null;
+			Data data = null;
+			try {
+				getData = zgif.getClass().getMethod("getData");
+				data = (Data) getData.invoke(zgif);
+			} catch (Exception e) {
+			}
+			if (data != null) {
+				exportData(data, "maindata.xml");
+			}
 
-            Method getPeriods = null;
-            Map<String, Period> periods = null;
-            try {
-                getPeriods = zgif.getClass().getMethod("getPeriods");
-                periods = (Map<String, Period>) getPeriods.invoke(zgif);
-            } catch (Exception e) {
-            }
-            exportPeriods(periods.values());
+			Method getPeriods = null;
+			Map<String, Period> periods = null;
+			try {
+				getPeriods = zgif.getClass().getMethod("getPeriods");
+				periods = (Map<String, Period>) getPeriods.invoke(zgif);
+			} catch (Exception e) {
+			}
+			exportPeriods(periods.values());
 
-            if (data == null && periods == null) {
-                logger.error("data and periods of zgif is null!");
-            }
+			if (data == null && periods == null) {
+				logger.error("data and periods of zgif is null!");
+			}
 
-            nextZipEntry("META-INF/manifest.xml");
-            ManifestWriter manifestWriter = new ManifestWriter(zipOut);
-            manifestWriter.write(fileList);
+			nextZipEntry("META-INF/manifest.xml");
+			ManifestWriter manifestWriter = new ManifestWriter(zipOut);
+			manifestWriter.write(fileList);
 
-            zipOut.closeEntry();
-            zipOut.close();
-        } catch (IOException e) {
-            logger.error("Error writing zgif file", e);
-        }
-    }
+			zipOut.closeEntry();
+			zipOut.close();
+		} catch (IOException e) {
+			logger.error("Error writing zgif file", e);
+		}
+	}
 
-    private void nextZipEntry(String path) throws IOException {
-        zipOut.putNextEntry(new ZipEntry(path));
-        fileList.add(path);
-    }
+	private void nextZipEntry(String path) throws IOException {
+		zipOut.putNextEntry(new ZipEntry(path));
+		fileList.add(path);
+	}
 
-    private void exportData(Data data, String path) throws IOException {
-        nextZipEntry(path);
-        DataWriter dataWriter = new DataWriter(zipOut);
-        dataWriter.write(data);
-    }
+	private void exportData(Data data, String path) throws IOException {
+		nextZipEntry(path);
+		DataWriter dataWriter = new DataWriter(zipOut);
+		dataWriter.write(data);
+	}
 
-    private void exportPeriods(Collection<Period> periods) throws IOException {
+	private void exportPeriods(Collection<Period> periods) throws IOException {
 
-        for (Period period : periods) {
-            nextZipEntry("periods/" + period.getIdentifier() + ".xml");
-            // DataWriter dataWriter = new DataWriter(zipOut);
-            // TODO
-            // dataWriter.write(period.getData());
-        }
+		for (Period period : periods) {
+			nextZipEntry("periods/" + period.getIdentifier() + ".xml");
+			// DataWriter dataWriter = new DataWriter(zipOut);
+			// TODO
+			// dataWriter.write(period.getData());
+		}
 
-        nextZipEntry("periods.xml");
-        new PeriodsWriter(zipOut).write(periods);
+		nextZipEntry("periods.xml");
+		new PeriodsWriter(zipOut).write(periods);
 
-    }
+	}
 }
